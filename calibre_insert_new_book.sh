dd99bd02dc6d39744c7bbf07c4df627d66368568
latty <lionel.atty@gmail.com>
2017-08-03 15:05:21 +0200

    rajout de documentation au script.

--- calibre_insert_new_book.sh
+++ calibre_insert_new_book.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
 
+# https://manual.calibre-ebook.com/generated/en/calibredb.html
+# https://stackoverflow.com/questions/15580144/how-to-concatenate-multiple-lines-of-output-to-one-line
 calibredb add `find epub/ -maxdepth 1 -type f -iname '*.epub' -mtime -1 | tr '\n' ' '`
